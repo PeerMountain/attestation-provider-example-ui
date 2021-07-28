403761ad322be0c92c5adfde4441dd7a06dd4cdb
Igor Drozdov <igor@kyc3.com>
2021-07-28 12:24:58 +0200

    [PEER-103] Show attestation data to a user

--- src/app/token-uri-access/token-uri-access.component.spec.ts
+++ src/app/token-uri-access/token-uri-access.component.spec.ts
@@ -1,16 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TokenUriAccessComponent } from './token-uri-access.component';
+import {TokenUriAccessComponent} from './token-uri-access.component';
+import {ActivatedRoute} from "@angular/router";
+import {provideMockStore} from "@ngrx/store/testing";
+import {initialAttestationState} from "../reducers/attestation.reducer";
 
 describe('TokenUriAccessComponent', () => {
   let component: TokenUriAccessComponent;
   let fixture: ComponentFixture<TokenUriAccessComponent>;
+  const activatedRouteMock = {
+    snapshot: {
+      paramMap: new Map([
+        ["token", "token"]
+      ])
+    }
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TokenUriAccessComponent ]
+      declarations: [TokenUriAccessComponent],
+      providers: [
+        {provide: ActivatedRoute, useValue: activatedRouteMock},
+        provideMockStore({
+            initialState: {
+              attestation: initialAttestationState
+            }
+          }
+        )
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
